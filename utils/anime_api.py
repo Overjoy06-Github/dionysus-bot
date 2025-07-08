afebdfdc09e992a2bc9cbd8d8aeede2d900e19f0
Overjoy06-Github <56468229+Overjoy06-Github@users.noreply.github.com>
2025-07-08 17:26:23 +0800

    Update anime_api.py

--- utils/anime_api.py
+++ utils/anime_api.py
@@ -1,23 +1,26 @@
-import requests
-import random
 from .cache import get_top_anime
+import aiohttp
+import random
 
-def search_anime(query, limit=5):
+async def search_anime(query, limit=5):
     try:
         url = f"https://api.jikan.moe/v4/anime?q={query}&limit={limit}"
-        response = requests.get(url, timeout=10)
-        response.raise_for_status()
-        
-        data = response.json().get('data', [])
-        return [{
-            'title': anime.get('title', 'Unknown Title'),
-            'score': anime.get('score', 'N/A'),
-            'episodes': anime.get('episodes', 'Null'),
-            'synopsis': anime.get('synopsis', 'Unknown'),
-            'genres': ', '.join(g['name'] for g in anime.get('genres', [])),
-            'image_url': anime.get('images', {}).get('jpg', {}).get('image_url', ''),
-            'anime_url': anime.get('url', 'https://.')
-        } for anime in data]
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, timeout=10) as response:
+                response.raise_for_status()
+                data = await response.json()
+                
+                anime_list = data.get('data', [])
+                return "None" if not anime_list else [{
+                    'title': anime.get('title', 'Unknown Title'),
+                    'type': anime.get('type', 'N/A'),
+                    'score': anime.get('score', 'N/A'),
+                    'episodes': anime.get('episodes', 'Null'),
+                    'synopsis': anime.get('synopsis', 'Unknown'),
+                    'genres': ', '.join(g['name'] for g in anime.get('genres', [])) if anime.get('genres') else 'None',
+                    'image_url': anime.get('images', {}).get('jpg', {}).get('image_url', ''),
+                    'anime_url': anime.get('url', 'https://.')
+                } for anime in anime_list]
     except:
         return []
 
@@ -30,6 +33,7 @@ async def fetch_valid_anime():
         anime = random.choice(top_anime)
         return {
             'title': anime.get('title', 'Unknown'),
+            'type': anime.get('type', 'Unknown'),
             'title_english': anime.get('title_english', ''),
             'score': anime.get('score', 'N/A'),
             'episodes': anime.get('episodes', 'Unknown'),
